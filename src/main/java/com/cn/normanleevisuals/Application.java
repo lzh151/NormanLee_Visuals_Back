b0cf6463c79fceee178e10e98b23661489693680
lzh151 <lzh151@qq.com>
2021-07-01 17:02:49 +0100

    Add swagger2

--- src/main/java/com/cn/normanleevisuals/Application.java
+++ src/main/java/com/cn/normanleevisuals/Application.java
@@ -3,8 +3,10 @@ package com.cn.normanleevisuals;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @MapperScan("com.cn.normanleevisuals.dao")
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/cn/normanleevisuals/controller/HelloController.java
+++ src/main/java/com/cn/normanleevisuals/controller/HelloController.java
@@ -1,16 +1,26 @@
 package com.cn.normanleevisuals.controller;
 
 import com.cn.normanleevisuals.dao.UserDao;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "用户操作")
 @RestController
 public class HelloController {
 
     @Autowired
     UserDao userDao;
 
+    @ApiOperation(value="用户登录", notes="根据用户姓名密码登录")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType="query", name = "userName", value = "用户姓名", required = true, dataType = "String"),
+            @ApiImplicitParam(paramType="query", name = "password", value = "密码", required = true, dataType = "String")
+    })
     @RequestMapping("/index")
     public String index(String userName, String password) {
         System.out.println("用户名：" + userName);
